37cbe4db207299a17c7fb5f459abb5a342d9e2df
Matthew Martinez <matt.martinez2120@gmail.com>
2022-01-31 19:15:00 -0600

    re-typed exercise

---
+++ js/external.js
@@ -0,0 +1,45 @@
+console.log("Hello From External JavaScript")
+// JavaScript code goes here
+// alert("Welcome To My Website!");
+//
+// prompt("What Is Your Favorite Color?");
+//
+// alert("Blue Is My Favorite Color Too!");
+
+// alert("Welcome to my Website!");
+
+// var favoriteColor = prompt("What's your favorite color?");
+// alert("Great, " + favoriteColor + " is my favorite color too!");
+
+// var pricePerDayInDollars = 3;
+// var daysOfLittleMermaid = Number(prompt("For how many days are you renting The Little Mermaid?"));
+// var daysOfBrotherBear = Number(prompt("For how many days are you renting Brother Bear?"));
+// var daysOfHercules = Number(prompt("For how many days are you renting Hercules?"));
+//var total = (daysOfLittleMermaid + daysOfBrotherBear + daysOfHercules) * pricePerDayInDollars;
+// alert("Your total will be $" + total);
+
+// var whatGooglePaysInDollarsPerHour = Number(prompt("How much does Google pay you per hour?"));
+// var hoursWorkedForGoogle = Number(prompt("How many hours did you work for Google this week?"));
+// var whatAmazonPaysInDollarsPerHour = Number(prompt("How much does Amazon pay you per hour?"));
+// var hoursWorkedForAmazon = Number(prompt("How many hours did you work for Amazon this week?"));
+// var whatFacebookPaysInDollarsPerHour = Number(prompt("How much does Facebook pay you per hour?"));
+// var hoursWorkedForFacebook = Number(prompt("How many hours did you work for Facebook this week?"));
+// var myMoneyThisWeek = (whatGooglePaysInDollarsPerHour * hoursWorkedForGoogle) + (whatAmazonPaysInDollarsPerHour * hoursWorkedForAmazon) + (whatFacebookPaysInDollarsPerHour * hoursWorkedForFacebook);
+// alert("You made " + myMoneyThisWeek + " dollars this week!");
+
+var classNotFull = confirm("Is the class full?");
+var noScheduleConflict = confirm("Do you have a schedule conflict?");
+var canEnroll = (classNotFull && noScheduleConflict);
+alert("It is " + canEnroll + " that you can enroll in this class");
+
+
+
+
+
+
+
+
+
+
+
+
