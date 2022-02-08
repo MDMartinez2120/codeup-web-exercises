a39076e09e25dd8241a7b4405c89441997633af9
Matthew Martinez <matt.martinez2120@gmail.com>
2022-02-07 21:42:09 -0600

    finished the for loops exercises

---
+++ js/for_loops.js
@@ -0,0 +1,31 @@
+// function showMultiplicationTable(number) {
+//     console.log(number)
+// }
+
+// for (var number = 1; number <=10; number++) {
+//     var result = 7 * number;
+//     console.log(`7 * ${number} = ${result}`);
+// }
+
+
+
+
+// var randomNumber = Math.floor(Math.random())
+//
+// for (var number = 1; number <=10; number++) {
+//     var randomNumber = Math.floor(Math.random() * 200 - 20 + 1 + 20);
+//     if (randomNumber & 2 === 0) {
+//         console.log(randomNumber+ ' is even')
+//     } else {
+//         console.log(randomNumber+ ' is odd')
+//     }
+// }
+
+// for (var repeatNumber = 1; repeatNumber <= 9; repeatNumber++) {
+//     console.log(repeatNumber.toString().repeat(repeatNumber));
+// }
+
+for (var byFive = 100; byFive > 5; byFive -= 5) {
+    console.log(byFive);
+}
+
--- js/while.js
+++ js/while.js
@@ -6,20 +6,20 @@
 //
 // }
 
-var amountOfCones = Math.floor(Math.random() * 50) + 50;
-var conesBought;
-
-do {
-    if (amountOfCones >= 1) {
-        console.log(amountOfCones + " we got you")
-    } else if (amountOfCones === 0) {
-        console.log(amountOfCones + " im all out baby")
-    }
-    conesBought = Math.floor(Math.random() * 5) +1;
-    console.log('how many cones are left ? ' + (amountOfCones - conesBought));
-    amountOfCones = amountOfCones - conesBought;
-
-} while (amountOfCones >= 1);
+// var amountOfCones = Math.floor(Math.random() * 50) + 50;
+// var conesBought;
+//
+// do {
+//     if (amountOfCones >= 1) {
+//         console.log(amountOfCones + " we got you")
+//     } else if (amountOfCones === 0) {
+//         console.log(amountOfCones + " im all out baby")
+//     }
+//     conesBought = Math.floor(Math.random() * 5) +1;
+//     console.log('how many cones are left? ' + (amountOfCones - conesBought));
+//     amountOfCones = amountOfCones - conesBought;
+//
+// } while (amountOfCones >= 1);
 
 
 
