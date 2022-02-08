da1abfa7652dda338b4a3f504f592a19f14717e0
Matthew Martinez <matt.martinez2120@gmail.com>
2022-02-08 09:31:48 -0600

    worked on morning exercise.

--- js/js-warmups.js
+++ js/js-warmups.js
@@ -1,14 +1,37 @@
 
 
-function returnStatement(string) {
-    return 'i have no idea what you just said';
+// function returnStatement(string) {
+//     return 'i have no idea what you just said';
+// }
+//
+// console.log(returnStatement('string'))
+//
+//
+// function returnNumber(userNumber) {
+//     return userNumber * 2;
+// }
+//
+// console.log(returnNumber(5))
+
+// function aNumber(number){
+//     return number + number;
+// }
+//
+// aNumber()
+// console.log(aNumber(6))
+
+// function twoNumbers(number, number) {
+//     return number * 2 + number * 2;
+// }
+//
+// twoNumbers()
+// console.log(twoNumbers(6, 8))
+
+function inNumber(number) {
+    return number * 5 ;
 }
 
-console.log(returnStatement('string'))
+inNumber()
+console.log()
 
 
-function returnNumber(userNumber) {
-    return userNumber * 2;
-}
-
-console.log(returnNumber(5))
