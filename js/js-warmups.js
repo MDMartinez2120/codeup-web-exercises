6eea2bb1635048733d793721b6ddfcfd65bfdead
Matthew Martinez <matt.martinez2120@gmail.com>
2022-03-02 11:51:10 -0600

    Finished the dom-query exercise.

--- js/js-warmups.js
+++ js/js-warmups.js
@@ -163,20 +163,22 @@
 //         return '' + totalOverM + '/' + lengthOfString;
 // }
 
-function isLockNessMonster(s){
-    if (s.includes('tree fiddy')){
-        return true;
-    }else {
-        return false;
-    }
-}
+// function isLockNessMonster(s){
+//     if (s.includes('tree fiddy')){
+//         return true;
+//     }else {
+//         return false;
+//     }
+// }
+//
+//
+// console.log(isLockNessMonster("Your girlscout cookies are ready to ship. Your total comes to tree fiddy"));
+//
+// console.log(isLockNessMonster("Your girlscout cookies are ready to ship. Your total comes to tree fiddy"));
+//
+// console.log(isLockNessMonster("Yo, I heard you were on the lookout for Nessie. Let me know if you need assistance."));
+//
+// console.log(isLockNessMonster("Yo, I heard you were on the lookout for Nessie. Let me know if you need assistance."));
 
 
-console.log(isLockNessMonster("Your girlscout cookies are ready to ship. Your total comes to tree fiddy"));
-
-console.log(isLockNessMonster("Your girlscout cookies are ready to ship. Your total comes to tree fiddy"));
-
-console.log(isLockNessMonster("Yo, I heard you were on the lookout for Nessie. Let me know if you need assistance."));
-
-console.log(isLockNessMonster("Yo, I heard you were on the lookout for Nessie. Let me know if you need assistance."));
 
