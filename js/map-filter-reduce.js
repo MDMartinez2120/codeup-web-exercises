1e97c0db0d09d389839017f182f286c20c120679
Matthew Martinez <matt.martinez2120@gmail.com>
2022-03-18 16:46:48 -0500

    finished the map-filter-reduce exercises.

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,96 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+
+// Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
+//
+const threeLanguages = person => person.languages.length >= 3
+
+const threeLanguageArray = users.filter(threeLanguages)
+
+console.log(threeLanguageArray)
+//
+//
+//
+//
+//
+// Use .map to create an array of strings where each element is a user's email address
+//
+const usersEmails = users.map((element) => element.email)
+
+console.log(usersEmails)
+//
+//
+//
+//
+//
+// Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
+//
+//
+const userExperience = users.reduce((previousValue, currentValue) => {
+    return previousValue +  currentValue.yearsOfExperience;
+}, 0);
+
+console.log(userExperience)
+//
+//
+//
+//
+//  Use .reduce to get the longest email from the list of users.
+//
+//
+const longestEmail = usersEmails.reduce((previousValue, currentValue) => {
+    if (previousValue.length > currentValue.length){
+        return currentValue;
+    }else {
+        return previousValue
+    }
+}, '');
+console.log(longestEmail)
+//
+//
+//
+//
+//
+//  Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
+
+const userNames = users.reduce((previousValue, currentValue) => {
+    return previousValue + currentValue.name
+}, '');
+
+console.log(userNames)
